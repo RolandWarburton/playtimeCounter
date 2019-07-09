a02f355db7e2bff7298e98c40eb6e9b91669f0bf
BuildTools <warburtonroland@gmail.com>
2019-07-09 17:53:05 +1000

    wrote how to convert sec to hours or days

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os
 import json
 
 __author__ = "Roland"
-__version__ = "1.0.2"
+__version__ = "1.0.1"
 
 
 def main():
 	# where to look for stats files
-	statsDir = './stats'
-	
+	statsDir = '/home/roland/minecraft/world/stats'
 	# where to save the results
 	outputFile = open("playtimeCounter.txt", "w")
 	# total in secconds: 1 minecraft min = 0.83 irl secconds
@@ -17,15 +16,16 @@ def main():
 	# change to the stats dir
 	os.chdir(statsDir)
 
-	for i,filename in enumerate(os.listdir(statsDir)):
-		print('FILE ' + str(i) + ' ' + filename)
+	for filename in os.listdir(statsDir):
 		f = open(filename,'r')
 		data = json.load(f)
 		total += data['stats']['minecraft:custom']['minecraft:play_one_minute']
-	
-	# divide into irl secconds then convert to days. 86400 is the seccond to day formula
-	total = (total/72)/86400
-	print('the total is: %d days' % (total) )
+
+	# divide into irl secconds then convert to days. 
+	# 86400 = seccond to day formula
+	# 3600 = seccond to hour formula
+	total = (total/72)/3600
+	# print('the total is: %d days' % (total) )
 	outputFile.write(str(total))
 
 
